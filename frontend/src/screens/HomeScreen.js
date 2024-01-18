a592768d40624de310e1fccb6edb461dcf2f4fd5
Aravind <aravindparavind10@gmail.com>
2024-01-18 23:18:44 +0530

    fetch data from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import data from '../data'
+import React, { useEffect, useState } from 'react'
+// import data from '../data'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data)
+    }
+    fetchData();
+  }, [])
   return (
     <div>
       <h1>Featured Products</h1>
-          <div className="products">
-            {
-              data.products.map(product => (<div key={product.slug} className="product">
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-                <div className="product-info">
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                  <p><strong>${product.price}</strong></p>
-                  <button>Add to cart</button>
-                </div>
-              </div>))
-            }
-          </div>
+      <div className="products">
+        {
+          products.map(product => (<div key={product.slug} className="product">
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p><strong>${product.price}</strong></p>
+              <button>Add to cart</button>
+            </div>
+          </div>))
+        }
+      </div>
     </div>
   )
 }
