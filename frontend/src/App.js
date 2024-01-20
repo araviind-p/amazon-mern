03a94c2e587bef93da94f32e4a770618aea45a94
Aravind <aravindparavind10@gmail.com>
2024-01-20 20:34:58 +0530

    created product details screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Container from 'react-bootstrap/Container';
-import { Container, Navbar } from "react-bootstrap"
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
@@ -20,7 +19,7 @@ function App() {
           </Navbar>
         </header>
         <main>
-          <Container>
+          <Container className='mt-3'>
             <Routes>
               <Route path='/product/:slug' element={<ProductScreen />} />
               <Route path='/' element={<HomeScreen />} />
