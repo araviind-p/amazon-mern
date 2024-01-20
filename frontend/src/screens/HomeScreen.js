77361fdcaa930ce3e46194c12f451c464d7292dd
Aravind <aravindparavind10@gmail.com>
2024-01-20 22:12:17 +0530

    created loading and message components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,6 +6,8 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Product from '../components/Product';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -48,8 +50,8 @@ export default function HomeScreen() {
       <h1>Featured Products</h1>
       <div className="products">
         {
-          loading ? (<div>Loading...</div>)
-            : error ? (<div>{error}</div>)
+          loading ? (<LoadingBox />)
+            : error ? (<MessageBox variazt="danger">{error}</MessageBox>)
               : (<Row>
                 {products.map(product => (
                   <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,7 +9,9 @@ import Card from 'react-bootstrap/Card';
 import Rating from '../components/Rating';
 import Button from 'react-bootstrap/esm/Button';
 import { Helmet } from 'react-helmet-async';
-import Product from './../components/Product';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -39,14 +41,14 @@ export default function ProductScreen() {
                 const result = await axios.get(`/api/products/slug/${slug}`);
                 dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
             } catch (err) {
-                dispatch({ type: 'FETCH_FAIL', payload: err.message });
+                dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
             }
         }
         fetchData();
     }, [slug])
     return (
-        loading ? <div>Loading...</div>
-            : error ? <div>{error}</div>
+        loading ? (<LoadingBox />)
+            : error ? (<MessageBox variant="danger">{error}</MessageBox>)
                 :
                 <div>
                     <Row>
@@ -101,7 +103,7 @@ export default function ProductScreen() {
                                                 }</Col>
                                             </Row>
                                         </ListGroup.Item>
-                                        {product.countInStock > 0 &&(
+                                        {product.countInStock > 0 && (
                                             <ListGroup.Item>
                                                 <div className='d-grid'>
                                                     <Button variant='primary'>
