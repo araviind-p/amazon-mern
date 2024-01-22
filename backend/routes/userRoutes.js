9f2aacd31e652576c3a1f78ab7682f0020236baf
Aravind <aravindparavind10@gmail.com>
2024-01-22 21:34:31 +0530

    create user profile update screen

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import User from '../models/userModel.js';
-import { generateToken } from '../utils.js';
+import { generateToken, isAuth } from '../utils.js';
 import bcrypt from 'bcryptjs';
 import expressAsyncHandler from 'express-async-handler'
 
@@ -40,5 +40,28 @@ userRouter.post('/signup',
         });
     })
 )
+
+userRouter.put('/profile',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+        const user = await User.findById(req.user._id);
+        if (user) {
+            user.name = req.body.name || user.name;
+            user.email = req.body.email || user.email;
+            if (req.body.password) {
+                user.password = bcrypt.hashSync(req.body.password, 8);
+            }
+            const updatedUser = await user.save();
+            res.send({
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                isAdmin: updatedUser.isAdmin,
+                token: generateToken(updatedUser),
+            })
+        } else {
+            res.status(404).send({ message: 'User not found!' })
+        }
+    }))
 
 export default userRouter;
